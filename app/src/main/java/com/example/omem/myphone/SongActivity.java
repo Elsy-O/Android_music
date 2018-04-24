9bf2f56695debadbaecefd2a769133e77652955c
omem <elsy.ondo@gmail.com>
2018-04-24 04:54:31 +0200

    Initial commit : my first commit for this GitHub project

---
+++ app/src/main/java/com/example/omem/myphone/SongActivity.java
@@ -0,0 +1,21 @@
+package com.example.omem.myphone;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SongActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+        this.mediaPlayer.create(getApplicationContext(), R.raw.my_song_two);
+    }
+
+    public void playMyMusic(View view) {
+        mediaPlayer.start();
+    }
+}
